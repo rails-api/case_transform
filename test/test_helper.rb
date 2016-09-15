f23f3660d7ce835b4ba4a432d760d11084d09165
L. Preston Sego III <LPSego3+dev@gmail.com>
2016-09-15 16:17:30 -0400

    rubocop -a, cause I'm going too fast now

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 require 'bundler/setup'
 
-require "codeclimate-test-reporter"
+require 'codeclimate-test-reporter'
 
-if ENV['CODECLIMATE_REPO_TOKEN']
-  CodeClimate::TestReporter.start
-end
-
+CodeClimate::TestReporter.start if ENV['CODECLIMATE_REPO_TOKEN']
 
 require 'pry-byebug'
 require 'case_transform'
