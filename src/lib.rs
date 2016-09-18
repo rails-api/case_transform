c01ada82e6d71c59e497f197a5d0c6466a62d206
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-09-18 07:49:42 -0400

    rust is trying to build.. but RubyString doesn't have to_string()

--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,32 @@
 extern crate helix;
 extern crate inflector;
 
-// dash: kebab-case
-use inflector::cases::kebabcase::to_kebab_case;
-// underscore: snake_case
-use inflector::cases::snakecase::to_snake_case;
-// camel_lower: camelCase
-use inflector::cases::camelcase::to_camel_case;
-// camel: ClassCase (PascalCase)
-use inflector::cases::classcase::to_class_case;
+// // dash: kebab-case
+// use inflector::cases::kebabcase::to_kebab_case;
+// // underscore: snake_case
+// use inflector::cases::snakecase::to_snake_case;
+// // camel_lower: camelCase
+// use inflector::cases::camelcase::to_camel_case;
+// // camel: ClassCase (PascalCase)
+// use inflector::cases::classcase::to_class_case;
+use inflector::Inflector;
 
 declare_types! {
     reopen class RubyString {
         def to_snake_case(self) -> String {
-            to_snake_case(self.to_string());
+            self.to_string().to_snake_case();
         }
 
         def to_camel_case(self) -> String {
-            to_camel_case(self.to_string());
+            self.to_string().to_camel_case();
         }
 
         def to_class_case(self) -> String {
-            to_class_case(self.to_string());
+            self.to_string().to_class_case();
         }
 
         def to_kebab_case(self) -> String {
-            to_kebab_case(self.to_string());
+            self.to_string().to_kebab_case();
         }
     }
 }
