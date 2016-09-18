a62f1492925be3994a95b5248bc6919d0cf6c6a8
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-09-18 07:53:44 -0400

    successful cargo build?

--- src/lib.rs
+++ src/lib.rs
@@ -15,19 +15,30 @@ use inflector::Inflector;
 declare_types! {
     reopen class RubyString {
         def to_snake_case(self) -> String {
-            self.to_string().to_snake_case();
+            self.to_string().to_snake_case()
         }
 
         def to_camel_case(self) -> String {
-            self.to_string().to_camel_case();
+            self.to_string().to_camel_case()
         }
 
         def to_class_case(self) -> String {
-            self.to_string().to_class_case();
+            self.to_string().to_class_case()
         }
 
         def to_kebab_case(self) -> String {
-            self.to_string().to_kebab_case();
+            self.to_string().to_kebab_case()
         }
+    }
+}
+
+
+// TODO: Helix is eventually going to implement this
+use helix::{UncheckedValue, ToRust};
+
+impl ToString for RubyString {
+    fn to_string(&self) -> String {
+        let checked = self.0.to_checked().unwrap();
+        checked.to_rust()
     }
 }
