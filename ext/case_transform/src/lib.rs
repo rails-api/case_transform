45e053b87f0ebb5ea16f77d5535ff44ec0245bc1
NullVoxPopuli <LPSego3+dev@gmail.com>
2016-09-20 22:10:06 -0400

    try to convert to pure rust -- all the errors

--- ext/case_transform/src/lib.rs
+++ ext/case_transform/src/lib.rs
@@ -3,56 +3,68 @@ extern crate ruru;
 extern crate inflector;
 
 // // dash: kebab-case
-// use inflector::cases::kebabcase::to_kebab_case;
+use inflector::cases::kebabcase::to_kebab_case;
 // // underscore: snake_case
-// use inflector::cases::snakecase::to_snake_case;
+use inflector::cases::snakecase::to_snake_case;
 // // camel_lower: camelCase
-// use inflector::cases::camelcase::to_camel_case;
+use inflector::cases::camelcase::to_camel_case;
 // // camel: ClassCase (PascalCase)
-// use inflector::cases::classcase::to_class_case;
-use inflector::Inflector;
+use inflector::cases::classcase::to_class_case;
 
-use ruru::{Class, Object, RString, Hash};
+use ruru::{Class, Object, RString, Hash, Array, Symbol, AnyObject, VM};
+use ruru::types::ValueType;
+
+class!(CaseTransform);
 
 methods! (
-    RString,
+    CaseTransform,
     itself,
 
-    fn toSnakeCase() -> RString {
-        RString::new(&itself.to_string().to_snake_case())
+    fn deepTransformKeys(hash: Hash, block: &Fn(String) -> String) -> Hash {
+        let result = Hash::new();
+
+        hash.unwrap().each(|key, value| {
+            let newValue = if value.ty() == ValueType::Hash { deepTransformKeys(value, block).to_any_object() } else { value };
+            let newKey = RString::new(block(key.unwrap().to_string()));
+            result.store(newKey, newValue);
+        });
+
+        result
     }
 
-    fn toCamelCase() -> RString {
-        RString::new(&itself.to_string().to_camel_case())
+    fn transform(
+        value: AnyObject,
+        objectTransform: &Fn(AnyObject) -> AnyObject,
+        keyTransform: &Fn(String) -> String
+    ) -> AnyObject {
+        match value.unwrap().ty() {
+            ValueType::Array => value.map(|item| objectTransform(item)).to_any_object(),
+            ValueType::Hash => deepTransformKeys(value, &|key| objectTransform(key)).to_any_object(),
+            ValueType::Symbol => Symbol::new(objectTransform(value.to_string)).to_any_object(),
+            ValueType::RString => keyTransform(value).to_any_object(),
+            ValueType::Object => value
+        }
     }
 
-    fn toClassCase() -> RString {
-        RString::new(&itself.to_string().to_class_case())
-    }
+    fn toPascalCase(key: String) -> String { to_class_case(to_snake_case(key.unwrap())) }
+    fn toCamelCase(key: String) -> String { to_camel_case(to_snake_case(key.unwrap())) }
+    fn toDashedCase(key: String) -> String { to_kebab_case(to_snake_case(key.unwrap())) }
+    fn toKebabCase(key: String) -> String { to_kebab_case(to_snake_case(key.unwrap())) }
 
-    fn toKebabCase() -> RString {
-        RString::new(&itself.to_string().to_kebab_case())
-    }
+    fn camel(value: AnyObject) -> AnyObject { transform(value.unwrap().to_any_object(), &camel, &toPascalCase) }
+    fn camelLower(value: AnyObject) -> AnyObject { transform(value.unwrap().to_any_object(), &camelLower, &toCamelCase) }
+    fn dash(value: AnyObject) -> AnyObject { transform(value.unwrap().to_any_object(), &dash, &toDashedCase) }
+    fn underscore(value: AnyObject) -> AnyObject { transform(value.unwrap(), &underscore, &toKebabCase) }
+    fn unaltered(value: AnyObject) -> AnyObject { value.unwrap().to_any_object() }
 );
-
-// methods! (
-//     Hash,
-//     itself,
-//
-//     fn deepTransformKeys() -> Hash {
-//         let result = Hash::new()
-//         itself.each(|key, value| {
-//             result.store(key, value)
-//         });
-//     }
-// );
 
 #[no_mangle]
 pub extern fn initialize_case_transform() {
-    Class::from_existing("String").define(|itself| {
-        itself.def("to_snake_case", toSnakeCase);
-        itself.def("to_camel_case", toCamelCase);
-        itself.def("to_class_case", toClassCase);
-        itself.def("to_kebab_case", toKebabCase);
+    Class::new("CaseTransform", None).define(|itself| {
+        itself.def_self("camel", camel);
+        itself.def_self("camel_lower", camelLower);
+        itself.def_self("dash", dash);
+        itself.def_self("underscore", underscore);
+        itself.def_self("unaltered", unaltered);
     });
 }
